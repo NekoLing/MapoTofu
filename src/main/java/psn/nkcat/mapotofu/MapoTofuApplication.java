5df284af38e2c668b2a1517d6261e3446dbf8dda
“NekoLing” <gaoqi135@icloud.com>
2022-05-31 23:08:25 +0800

    项目初始化

---
+++ src/main/java/psn/nkcat/mapotofu/MapoTofuApplication.java
@@ -0,0 +1,13 @@
+package psn.nkcat.mapotofu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapoTofuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapoTofuApplication.class, args);
+    }
+
+}
